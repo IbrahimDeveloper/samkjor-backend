3d608013539405da0220d4af5068872f156fa100
IbrahimDeveloper <bilakaunta23@gmail.com>
2026-06-13 22:01:11 +0200

    Fix SSL in migrate script

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,4 +1,10 @@
-import { pool } from "./index";
+import { Pool } from "pg";
+import "dotenv/config";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
 import fs from "fs";
 import path from "path";
 
